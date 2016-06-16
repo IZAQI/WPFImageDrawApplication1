afe144055dc7e9ffeb19c239f0a8efd1d94ec955
IZAQI <dbrmani@yahoo.co.jp>
2016-06-17 00:14:32 +0900

    many draw viewbox by one image.

--- WpfImageDrawApplication1/MainWindow.xaml.cs
+++ WpfImageDrawApplication1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +24,66 @@ namespace WpfImageDrawApplication1
     {
         public MainWindow()
         {
+
             InitializeComponent();
+            // 
+            //this.Dispatcher.Invoke(() => {
+
+                var path = System.IO.Path.Combine(System.Environment.CurrentDirectory, @"image.png");
+
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.CreateOptions = BitmapCreateOptions.None;
+                image.UriSource = new Uri(path, UriKind.Absolute);
+                image.EndInit();
+                image.Freeze();
+
+                ImageCollection = new ObservableCollection<ImageSourceModel>();
+
+                for (int i = 0; i < 50000; i++)
+                {
+                    ImageCollection.Add(new ImageSourceModel(image));
+                }
+
+                DataContext = ImageCollection;
+           /// });
+
         }
+
+        public ObservableCollection<ImageSourceModel> ImageCollection
+        {
+            set;get;
+        }
+    }
+
+    //public class ImageSourceModel : INotifyPropertyChanged
+    public class ImageSourceModel
+    {
+        public ImageSourceModel(BitmapImage path)
+        {
+            Bitmap = path;
+        }
+
+        private BitmapImage _bitmapImage = new BitmapImage();
+        public BitmapImage Bitmap
+        {
+            get { return _bitmapImage; }
+            set
+            {
+                _bitmapImage = value;
+                //OnPropertyChanged("Bitmap");
+            }
+
+        }
+
+        #region INotifyPropertyChanged メンバ  
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
     }
 }
