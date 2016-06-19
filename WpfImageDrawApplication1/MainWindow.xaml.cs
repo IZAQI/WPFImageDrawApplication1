ea41c75407a6d4163c80300cd1bbdb727bdcfd03
IZAQI <dbrmani@yahoo.co.jp>
2016-06-20 00:41:51 +0900

    Use VirtualizingUniformSizeWrapPanel.

--- WpfImageDrawApplication1/MainWindow.xaml.cs
+++ WpfImageDrawApplication1/MainWindow.xaml.cs
@@ -41,12 +41,23 @@ namespace WpfImageDrawApplication1
 
                 ImageCollection = new ObservableCollection<ImageSourceModel>();
 
-                for (int i = 0; i < 50000; i++)
+                for (int i = 0; i < 5000; i++)
                 {
                     ImageCollection.Add(new ImageSourceModel(image));
                 }
 
-                DataContext = ImageCollection;
+                path = System.IO.Path.Combine(System.Environment.CurrentDirectory, @"image2.png");
+                var image2 = new BitmapImage();
+                image2.BeginInit();
+                image2.CacheOption = BitmapCacheOption.OnLoad;
+                image2.CreateOptions = BitmapCreateOptions.None;
+                image2.UriSource = new Uri(path, UriKind.Absolute);
+                image2.EndInit();
+                image2.Freeze();
+            ImageCollection.Add(new ImageSourceModel(image2));
+
+
+            DataContext = ImageCollection;
            /// });
 
         }
@@ -68,7 +79,23 @@ namespace WpfImageDrawApplication1
         private BitmapImage _bitmapImage = new BitmapImage();
         public BitmapImage Bitmap
         {
-            get { return _bitmapImage; }
+            get
+            {
+                /*
+                var path = System.IO.Path.Combine(System.Environment.CurrentDirectory, @"image.png");
+
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.CreateOptions = BitmapCreateOptions.None;
+                image.UriSource = new Uri(path, UriKind.Absolute);
+                image.EndInit();
+                image.Freeze();
+
+                _bitmapImage = image;
+                */
+                return _bitmapImage;
+            }
             set
             {
                 _bitmapImage = value;
